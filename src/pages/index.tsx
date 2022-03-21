5e7c41ae601fbbc05393948fa94d9e9fffede8f9
Kyle <kaplaugher94@gmail.com>
2022-03-21 18:38:40 -0500

    add random pokemon id fetcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
+import { getOptionsForVote } from '@/utils/getRandomPokemon';
 import { trpc } from '@/utils/trpc';
 
 function Home() {
+  const [first, second] = getOptionsForVote();
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is Rounder?</div>
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-16 h-16 bg-red-200">1</div>
+        <div className="w-16 h-16 bg-red-800">{first}</div>
         <div className="p-8">vs</div>
-        <div className="w-16 h-16 bg-red-200">2</div>
+        <div className="w-16 h-16 bg-red-800">{second}</div>
       </div>
     </div>
   );
