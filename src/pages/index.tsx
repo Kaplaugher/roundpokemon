d73f1747f0688d9b2d731e24f49ade6af2977d0a
Kyle <kaplaugher94@gmail.com>
2022-03-22 09:39:22 -0500

    setting up votes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,11 +11,16 @@ function Home() {
 
   console.log(firstPokemon.data);
   if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
+
+  const voteForRoundest = (selectedNumber: number) => {
+    // fire mutation to persist changes to
+    updateIds(getOptionsForVote());
+  };
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is Rounder?</div>
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-64 h-64 flex flex-col">
+        <div className="w-64 h-64 flex flex-col items-center">
           <img
             src={firstPokemon.data?.sprites.front_default}
             alt=""
@@ -24,9 +29,15 @@ function Home() {
           <div className="text-xl text-center capitalize mt-[-2rem]">
             {firstPokemon.data?.name}
           </div>
+          <button
+            onClick={() => voteForRoundest(first)}
+            className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Rounder
+          </button>
         </div>
         <div className="p-8">vs</div>
-        <div className="w-64 h-64 flex flex-col">
+        <div className="w-64 h-64 flex flex-col items-center">
           <img
             src={secondPokemon.data?.sprites.front_default}
             alt=""
@@ -35,6 +46,12 @@ function Home() {
           <div className="text-xl text-center capitalize mt-[-2rem]">
             {secondPokemon.data?.name}
           </div>
+          <button
+            onClick={() => voteForRoundest(second)}
+            className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Rounder
+          </button>
         </div>
       </div>
     </div>
