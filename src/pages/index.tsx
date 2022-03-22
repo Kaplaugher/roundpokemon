b0cea70d417e32e21bfc00966b722b036ed3f2a3
Kyle <kaplaugher94@gmail.com>
2022-03-21 19:09:05 -0500

    backend with pokemon data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,41 @@
 import { getOptionsForVote } from '@/utils/getRandomPokemon';
 import { trpc } from '@/utils/trpc';
+import { useState } from 'react';
 
 function Home() {
-  const [first, second] = getOptionsForVote();
+  const [ids, updateIds] = useState(getOptionsForVote());
+  const [first, second] = ids;
+
+  const firstPokemon = trpc.useQuery(['get-pokemon-by-id', { id: first }]);
+  const secondPokemon = trpc.useQuery(['get-pokemon-by-id', { id: second }]);
+
+  console.log(firstPokemon.data);
+  if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is Rounder?</div>
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-16 h-16 bg-red-800">{first}</div>
+        <div className="w-64 h-64 flex flex-col">
+          <img
+            src={firstPokemon.data?.sprites.front_default}
+            alt=""
+            className="w-full"
+          />
+          <div className="text-xl text-center capitalize mt-[-2rem]">
+            {firstPokemon.data?.name}
+          </div>
+        </div>
         <div className="p-8">vs</div>
-        <div className="w-16 h-16 bg-red-800">{second}</div>
+        <div className="w-64 h-64 flex flex-col">
+          <img
+            src={secondPokemon.data?.sprites.front_default}
+            alt=""
+            className="w-full"
+          />
+          <div className="text-xl text-center capitalize mt-[-2rem]">
+            {secondPokemon.data?.name}
+          </div>
+        </div>
       </div>
     </div>
   );
